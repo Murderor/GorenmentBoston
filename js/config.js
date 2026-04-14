c12428703f0e3fa1eae147534f4d07cfd7039dcf
Bisquit <48838995+Murderor@users.noreply.github.com>
2026-04-14 23:23:39 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,5 @@
 export const ENV = {
   supabaseUrl: 'https://wkmyefxaenklvchrjuxd.supabase.co',
   supabaseAnonKey: 'sb_publishable_laFi7Ym5-X4-UOs5J9UPYw_7QthzYDE',
-  getSiteUrl: () => {
-    // Для GitHub Pages в подпапке
-    if (window.location.hostname.includes('github.io')) {
-      return 'https://murderor.github.io/GorenmentBoston';
-    }
-    // Для локальной разработки
-    return 'http://127.0.0.1:5500';
-  }
+  getSiteUrl: () => 'https://murderor.github.io/GorenmentBoston'
 };
